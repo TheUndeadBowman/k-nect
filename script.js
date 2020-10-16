80aa563f3f4c91d9391ea4d845344c7d42e82c87
TheUndeadBowman <72938745+TheUndeadBowman@users.noreply.github.com>
2020-10-16 19:27:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Don't look at mah code it's disgusting k thx bye");
+
+function increase() {
+    score++;
+    document.getElementById("slapps").innerHTML = score;
+}
+
+let score = 0;
